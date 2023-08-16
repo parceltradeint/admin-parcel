228717f210c47838ed3b81f5b442b6bf4940e61e
Md. Nayem Khan <mail.nayemkhan@gmail.com>
2023-08-16 21:22:42 +0600

    fixed build issue

--- src/components/Module/InBound.jsx
+++ src/components/Module/InBound.jsx
@@ -200,16 +200,6 @@ const InBound = () => {
               />
             </div>
           </div>
-
-          <div className={"ml-4 "}>
-            <button
-              onClick={billNew}
-              type="button"
-              className="inline-flex items-center px-3 py-2.5 border border-transparent text-sm leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
-            >
-              Add New
-            </button>
-          </div>
         </div>
       </div>
       <div className=" mx-auto py-1">
