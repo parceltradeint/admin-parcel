f025a901bf48108ce00b2565d7e5352e4a15128b
Md. Nayem Khan <mail.nayemkhan@gmail.com>
2024-03-02 00:14:23 +0600

    update fireabase admin key

--- src/pages/api/firebaseAdmin.js
+++ src/pages/api/firebaseAdmin.js
@@ -6,8 +6,12 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   // Initialize Firebase Admin SDK with application default credentials
   admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      credential: admin.credential.cert({
+        "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
+      }),
+      // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
   });
 }
