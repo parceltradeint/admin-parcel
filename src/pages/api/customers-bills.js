72de2b6179392e64c0372d5767d98880d7a3bbc0
nayemk1999 <mail.nayemkhan@gmail.com>
2025-05-27 17:16:07 +0600

    Update customers-bills.js

--- src/pages/api/customers-bills.js
+++ src/pages/api/customers-bills.js
@@ -71,9 +71,9 @@ export default async function newShipmentBill(req, res) {
 
       const searchQuery = {
         $and: [
-          // {
-          //   balance: { $exists: true, $ne: 0 },
-          // },
+          {
+            balance: { $exists: true, $ne: 0 },
+          },
           {
             customerId: { $exists: true },
           },
